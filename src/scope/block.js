3354a37b82f0e50e6a6c4f1371cda68f4f37a46c
Joel Tovar <jatr160397@gmail.com>
2021-10-10 11:25:02 -0400

    Making clear the difference betweeen var and let

---
+++ src/scope/block.js
@@ -0,0 +1,41 @@
+const fruits = () => {
+  if (true) {
+    var fruits1 = "apple";
+    //let fruits2 = "banana";
+    const fruits3 = "kiwi";
+  }
+  console.log(fruits1);
+  //console.log(fruits2);
+  console.log(fruits3);
+};
+
+fruits();
+
+var x = 1;
+{
+  var x = 2;
+  console.log(x + " in the block");
+}
+console.log(x + " Outside of the block");
+
+// Is better only use 'let'
+const anotherFunction = () => {
+  for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 3000);
+  }
+};
+
+anotherFunction();
+
+// Really big difference using 'var' instead of 'let'
+const anotherFunction = () => {
+  for (var i = 0; i < 10; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 3000);
+  }
+};
+
+anotherFunction();
